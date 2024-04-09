81727a4d32fedcc4ac7fcce5be6b2f9cfcd3e72f
Beryl Koram <92120634+berylkor@users.noreply.github.com>
2024-04-09 21:04:07 +0000

    update "home favicon"

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="css/test.css">
-    <link rel="shortcut icon" href="../assets/favicon_io/favicon.ico" type="image/x-icon">
-    <link rel="manifest" href="../assets/favicon_io/site.webmanifest">
-    <link rel="shortcut icon" href="../assets/favicon_io/favicon-16x16.png" type="image/png" sizes="16x16">
-    <link rel="shortcut icon" href="../assets/favicon_io/favicon-32x32.png" type="image/png" sizes="32x32">
+    <link rel="shortcut icon" href="assets/favicon_io/favicon.ico" type="image/x-icon">
+    <link rel="manifest" href="assets/favicon_io/site.webmanifest">
+    <link rel="shortcut icon" href="assets/favicon_io/favicon-16x16.png" type="image/png" sizes="16x16">
+    <link rel="shortcut icon" href="assets/favicon_io/favicon-32x32.png" type="image/png" sizes="32x32">
 </head>
 <body>
     <div class="grid_container">
