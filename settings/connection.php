8f59d363c85cb869db32fc658d23e4650ef3a000
Beryl Koram <92120634+berylkor@users.noreply.github.com>
2024-04-09 09:14:45 +0000

    edit "connection"

--- settings/connection.php
+++ settings/connection.php
@@ -1,13 +1,15 @@
 <?php
 
 // variables for setting connection to databse
-$SERVER = "localhost";
-$USERNAME = "root";
+// $SERVER = "localhost";
+// $USERNAME = "root"; 4406
+$SERVER = "172.187.229.113";
+$USERNAME = "beryl";
 $PASSWORD = "";
 $DATABASE = "blog_mgt";
 
 // start connection to the databse
-$CON = mysqli_connect($SERVER, $USERNAME, $PASSWORD, $DATABASE, 4406);
+$CON = mysqli_connect($SERVER, $USERNAME, $PASSWORD, $DATABASE);
 
 // check the connection worked
 if (mysqli_connect_errno())
