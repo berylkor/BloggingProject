a69f776424b16f2d30c9963bf76ccb7d3da2e325
Beryl Koram <92120634+berylkor@users.noreply.github.com>
2024-04-09 18:22:25 +0000

    update login

--- login/login_view.php
+++ login/login_view.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['user_not_found'] ))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="../css/test.css">
-    <link rel="stylesheet" href="sweetalert2.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet">
     <link rel="shortcut icon" href="../assets/favicon_io/favicon.ico" type="image/x-icon">
     <link rel="manifest" href="../assets/favicon_io/site.webmanifest">
     <link rel="shortcut icon" href="../assets/favicon_io/favicon-16x16.png" type="image/png" sizes="16x16">
@@ -82,6 +82,6 @@ if (isset($_SESSION['user_not_found'] ))
     </div>
     
 </body>
-<script src="sweetalert2.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
 <script src="../js/login.js"></script>
 </html>
