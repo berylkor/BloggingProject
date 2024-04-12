33cd2038a52967ae34e7171f2dafdc98fd9fd52f
Beryl Koram <92120634+berylkor@users.noreply.github.com>
2024-04-12 14:28:03 +0000

    update goals

--- actions/get_goals_action.php
+++ actions/get_goals_action.php
@@ -4,18 +4,24 @@ include "../settings/connection.php";
 $countuser = $_SESSION["user_id"];
 
 // select the like and comment counts, like and comment goal where the user is the current user
- $numlikescomments = "SELECT COUNT(Likes.LikesID) AS countlikes, COUNT(Comment.CommentID) AS countComments, Users.goalLikes, Users.goalComment 
-                    FROM  Users JOIN  Post ON Users.UserID = Post.UnID LEFT JOIN  Likes ON Post.PostID = Likes.PID
-                    LEFT JOIN  Comment ON Post.PostID = Comment.PID WHERE  Users.UserID = '$currentuser' GROUP BY  Users.UserID";
+//  $numlikescomments = "SELECT COUNT(Likes.LikesID) AS countlikes, COUNT(Comment.CommentID) AS countComments, Users.goalLikes, Users.goalComment 
+//                     FROM  Users JOIN  Post ON Users.UserID = Post.UnID LEFT JOIN  Likes ON Post.PostID = Likes.PID
+//                     LEFT JOIN  Comment ON Post.PostID = Comment.PID WHERE  Users.UserID = '$currentuser' GROUP BY  Users.UserID";
+
+$numlikes = "SELECT Post.countLike AS countlikes, Users.goalLikes FROM Users JOIN Post ON Users.UserID = Post.UnID WHERE Users.UserID = '$currentuser' ORDER BY countlikes DESC LIMIT 1";
+$numcomments = "SELECT Post.countComment AS countComments, User.goalComment FROM Users JOIN Post ON Users.UserID = Post.UnID WHERE Users.UserID = '$currentuser' ORDER BY countComments DESC LIMIT 1";
 
 // execute the query
-$numlikescomments_sql = mysqli_query($CON, $numlikescomments);
+$numlikes_sql = mysqli_query($CON, $numlikes);
+$numcomments_sql = mysqli_query($CON, $numcomments);
 // fect the results
-$numlikescomments_info = mysqli_fetch_all($numlikescomments_sql, MYSQLI_ASSOC);
+$numlikes_info = mysqli_fetch_all($numlikes_sql, MYSQLI_ASSOC);
+$numcomments_info = mysqli_fetch_all($numcomments_sql, MYSQLI_ASSOC);
 
 function getlikescomments()
 {
-    global $numlikescomments_info;
+    global $numlikes_info, $numcomments_info;
+    $numlikescomments_info = array_merge($numlikes_info, $numcomments_info);
     return $numlikescomments_info;
 }
 ?>
